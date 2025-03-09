a49772554605df58c02e8dc42da1c41657852ae1
Patrick Callahan <patricktcallahan18@gmail.com>
2025-03-09 08:56:26 +0100

    Create basic template code for getting a repo archive location

---
+++ backup_repo_template.R
@@ -0,0 +1,38 @@
+
+repos <- gh::gh(
+  "/orgs/{org}/repos",
+  org = "lmu-osc",
+  type = "all",
+  per_page = 100,
+  .limit = Inf
+) |>
+  purrr::map_chr("name")
+
+
+migration_ex <- gh::gh(
+  "POST /orgs/{org}/migrations",
+  org = "lmu-osc",
+  repositories = as.list("introduction-to-renv")
+)
+
+
+
+final_url <- sprintf("%s/archive", migration_ex[["url"]])
+
+
+handle <- curl::handle_setheaders(
+  curl::new_handle(followlocation = FALSE),
+  "Authorization" = paste("token", Sys.getenv("GITHUB_LMU_OSC_PAT")),
+  "Accept" = "application/vnd.github.v3+json"
+)
+
+
+check <- curl::curl_fetch_memory(
+  url = final_url,
+  handle = handle
+)
+
+
+parsed_headers <- curl::parse_headers_list(check$headers)
+
+parsed_headers$location

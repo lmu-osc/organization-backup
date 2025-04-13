add41b003f24000d40144b5f64d67e2d9c76b3e3
Patrick Callahan <patricktcallahan18@gmail.com>
2025-04-13 16:26:49 +0200

    Simplify check for deleting old backups

--- remove_old_backups.R
+++ remove_old_backups.R
@@ -4,45 +4,40 @@ try(renv::restore(prompt = FALSE))
 try(renv::restore(project = "archiving_code", prompt = FALSE))
 library(magrittr)
 
-# remove back up files created more than 365 days ago
 
+max_backup_life_days <- 365
+max_days_before_notify_backup_failure <- 14
+
+latest_backup_date <- as.Date(min(file.info(paste0("archive/", all_backups))$ctime))
+all_backups <- list.files("archive")
+
+print(paste("The latest backup date was on", latest_backup_date))
 print(paste("Running remove_old_backups.R script on", Sys.time()))
 
-all_backups <- list.files("archive")
+
+if (latest_backup_date <= (Sys.Date() - max_days_before_notify_backup_failure)) {
+  stop("The latest backup is more than 14 days old. Please check the backups and whether the GITHUB PAT has expired. Backup removal script has not been completed.")
+}
+
+
 
 old_backups <- all_backups %>%
   purrr::keep(~ {
-    as.Date(file.info(paste0("archive/", .x))$ctime) < (Sys.Date() - 365)
+    as.Date(file.info(paste0("archive/", .x))$ctime) < (Sys.Date() - max_backup_life_days)
   })
 
 print(paste("Old backups found: ", old_backups))
 
-
-# emergency_backups
-
-latest_backup_date <- as.Date(min(file.info(paste0("archive/", all_backups))$ctime))
-
-if (latest_backup_date < (Sys.Date() - 180)) {
-  print("Warning! The latest backup is more than 180 days old. Please check the backups and whether the GITHUB PAT has expired. The script will not remove any backups.")
-  emergency_backups <- old_backups
-} else {
-  emergency_backups <- ""
-}
-
-
-backups_to_remove <- old_backups %>%
-  setdiff(emergency_backups)
-
-
-
-if (!length(backups_to_remove)) {
+if (!length(old_backups)) {
   print("No backups to remove")
 } else {
   print("Removing old backups")
-  purrr::walk(~ {
+  purrr::walk(old_backups, ~ {
     print(paste0("Removing ", .x))
     file.remove(paste0("archive/", .x))
   })
 }
+
+
 
 
